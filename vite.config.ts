d30a7ca942c2cc7cb127664ace911d5f7dc2d407
Justin Dragos <justin.dragos@gmail.com>
2025-02-12 14:16:51 -0700

    Update to export esm modules. Fix types. Stop generating into `src` on build

--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,19 @@ const getPackageName = () => {
   return packageJson.name;
 };
 
-const fileName = {
-  es: `${getPackageName()}.js`,
-};
-
-const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
-
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "./build/dist",
+    outDir: "dist",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: `step-forge`,
-      formats,
-      fileName: format => fileName[format],
+      name: "step-forge",
+      formats: ["es"],
+      fileName: () => "step-forge.js"
     },
+    rollupOptions: {
+      external: Object.keys(packageJson.dependencies || {})
+    }
   },
   test: {
     watch: false,
